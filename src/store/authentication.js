cbe27f396191cfe1771867bfc2aab7b89a39d516
Zied BEN TAHAR <zied.bentahar@younited-credit.fr>
2018-06-12 15:13:40 +0200

    fix: remove console.log

--- src/store/authentication.js
+++ src/store/authentication.js
@@ -7,7 +7,6 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-  console.log(action);
   switch (action.type) {
     case SET_ANTHENTICATION_STATUS:
       const { payload } = action;
